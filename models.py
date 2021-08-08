Fixed exploding gradient (loss function returned NaN)

models.py
@@ -4,6 +4,7 @@ import tensorflow as tf
 from tensorflow import keras
 from tensorflow.keras.models import Sequential
 from tensorflow.keras.layers import Dense
+from tensorflow.python.keras.layers.core import Dropout
 
 class Model:
     def __init__(self, num_inputs, num_outputs):
@@ -13,8 +14,8 @@ class Model:
 
     # takes training data and feeds it to the model
     def train(self, inputs, outputs):
-        inputs = np.array(inputs).reshape(-1, self.num_inputs)
-        outputs = np.array(outputs).reshape(-1, self.num_outputs)
+        inputs = np.array(inputs, dtype="object").reshape(-1, self.num_inputs).astype('float32')
+        outputs = np.array(outputs).reshape(-1, self.num_outputs).astype('float32')
         self.model.fit(inputs, outputs)
 
     def save_model(self, filename):
@@ -31,12 +32,12 @@ class Model:
         self.model.load_weights(filename + '.h5')
 
     def compile_model(self):
-        self.model.compile(optimizer="adam", loss='mean_squared_error', metrics=['accuracy', 'mean_squared_error'])
+        self.model.compile(optimizer="adam", loss='mean_squared_error', metrics=['mean_squared_error'])
 
     # turns a value into a one-hot, like for the dice value 2, the func
     # call would be to_categorical(6, 1) with return val [0, 0, 0, 0, 1, 0]
     def to_categorical(self, num_classes, category):
-        return list(tf.keras.utils.to_categorical(category, num_classes=num_classes, dtype=int))
+        return np.array(tf.keras.utils.to_categorical(category, num_classes=num_classes, dtype=int))
 
     def categorize_die(self, die):
         categorical_die = ma.masked_array([])
@@ -52,7 +53,9 @@ class Model:
 
     def normalize_score_fields(self, score_fields):
         max_scores = [5, 10, 15, 20, 25, 30, 12, 22, 18, 24, 15, 20, 28, 30, 50]
-        return ma.masked_array([ score / max_scores[idx] for idx, score in enumerate(score_fields) ])
+        for idx in range(len(score_fields)):
+            score_fields.data[idx] /= max_scores[idx]
+        return score_fields
 
 # Model predicting which of the 5 die is best to throw again.
 # input: 
@@ -67,7 +70,8 @@ class DiceThrowModel(Model):
 
         self.model = Sequential([
             Dense(units=self.num_inputs, input_shape=(self.num_inputs,), activation='relu'),
-            Dense(units=32, activation='relu'),
+            Dense(units=42, activation='relu'),
+            Dropout(0.5),
             Dense(units=32, activation='relu'),
             Dense(units=self.num_outputs, activation='linear'),
         ])
@@ -119,7 +123,8 @@ class DiceThrowModel(Model):
                 max_value = value
                 best_move = inputs["die"]
 
-        return best_move
+        die_to_throw = np.array([ idx for idx in range(len(best_move)) if best_move.mask[idx] is not ma.masked ])
+        return die_to_throw
 
     def save_model(self):
         return super().save_model('DiceThrowModel')
@@ -141,6 +146,7 @@ class ScoreLogModel(Model):
         self.model = Sequential([
             Dense(units=self.num_inputs, input_shape=(self.num_inputs,), activation='relu'),
             Dense(units=48, activation='relu'),
+            Dropout(0.5),
             Dense(units=32, activation='relu'),
             Dense(units=self.num_outputs, activation='linear'),
         ])

player.py
@@ -1,5 +1,6 @@
 from collections import Counter
 from random import randint
+import numpy as np
 import numpy.ma as ma
 
 # Defines a player in the game (a set of scores and actions to be performed on the scores)
@@ -28,7 +29,7 @@ class Player:
             if self.score_fields.mask[field_index] == False:
                 points = 0
                 if 0 <= field_index <= 5: # singles
-                    points = dice.count(field_index + 1) * (field_index + 1)
+                    points = np.count_nonzero(dice == field_index + 1, axis=0) * (field_index + 1)
                 if field_index == 6: # pair
                     points = find_highest_duplicate_dice(dice, 2) * 2
                 if field_index == 7: # two pair
@@ -43,10 +44,10 @@ class Player:
                 if field_index == 9: # four of a kind
                     points = find_highest_duplicate_dice(dice, 4) * 4
                 if field_index == 10: # small straigt
-                    if dice == [1, 2, 3, 4, 5]:
+                    if np.all(dice == np.array([1, 2, 3, 4, 5])):
                         points = 15
                 if field_index == 11: # large straigt
-                    if dice == [2, 3, 4, 5, 6]:
+                    if np.all(dice == np.array([2, 3, 4, 5, 6])):
                         points = 20
                 if field_index == 12: # full house
                     dup_1 = find_highest_duplicate_dice(dice, 3)
@@ -58,7 +59,7 @@ class Player:
                 if field_index == 13: # chance
                     points = sum(dice)
                 if field_index == 14: # yatzy
-                    if dice.count(dice[0]) == 5:
+                    if np.all(dice == dice[0]):
                         points = 50
                 
                 possible_moves.append((field_index, points))

yatzy.py
@@ -1,4 +1,5 @@
 from random import randint
+import numpy as np
 
 from history import History
 
@@ -6,7 +7,7 @@ from history import History
 class Yatzy:
     def __init__(self, players):
         self.players = players
-        self.die = [1, 1, 1, 1, 1]
+        self.die = np.array([1, 1, 1, 1, 1])
         self.history = History()
 
     # plays the game
@@ -103,6 +104,3 @@ class Yatzy:
                 'sixes', 'pair', 'two pair', 'three of a kind', 
                 'four of a kind', 'small straight', 'large straight', 
                 'full house', 'chance', 'yatzy'][field_index]
-
-    def max_points(self, field_index):
-        return [5, 10, 15, 20, 25, 30, 12, 22, 18, 24, 15, 20, 28, 30, 50][field_index]